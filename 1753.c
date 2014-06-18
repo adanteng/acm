6a01329088b766a58cd1985b562409b65d3459ac
lihao <lihao@jiasiji.com>
2014-06-18 21:27:47 +0800

    fac

--- 1753.c
+++ 1753.c
@@ -27,6 +27,8 @@ typedef struct comnode {
 
 int *com_index_ptr;
 
+int compute_com_count(int n, int k);
+
 comnode merge_node(struct comnode node1, struct comnode node2) {
     int len1 = node1.len, len2 = node2.len, *ptr1 = node1.ptr, *ptr2 = node2.ptr, i;
     int *result = (int *)malloc(sizeof(int)*(len1+len2));
@@ -46,6 +48,7 @@ comnode merge_node(struct comnode node1, struct comnode node2) {
 }
 
 
+/*
 comnode find_all_com(int a[], int len, int com_count, int s) {
 	if (com_count == 1) {
 		return &a[s];
@@ -67,6 +70,94 @@ comnode find_all_com(int a[], int len, int com_count, int s) {
 		} 
 	}
 }
+*/
+
+int *get_all_combine(int maxn, int n, int c, int s) {
+    int i, len=n-c+1, end=len+s, *res, pi=0;
+    if (c == 1) {
+        res = (int *)malloc(sizeof(int)*(end-s));
+        for (i=s; i<end; i++) {
+            *(res+(pi++)) = i+1;
+        }
+
+        /*
+        for (i=0; i<len-s; i++) {
+            printf("%d", *(res+i));
+        }
+        printf("\n");
+        */
+        return res;
+    }
+
+    int j, k, fac;
+    int *arrptr[len];
+    int arrlen[len];
+    int fi=0;
+    int nexts = s+1;
+    int nextn = n-1;
+    int nextc = c-1;    
+    pi=0;
+    for (i=s; i<end; i++) {
+        int ele = i+1;
+
+        printf("%d %d %d %d\n", maxn, nextn, nextc, nexts);
+        int *childres = get_all_combine(maxn, nextn, nextc, nexts);
+        int childfac = compute_com_count(nextn, nextc);
+        int *p = (int *)malloc(sizeof(int)*c*childfac);
+
+        fi = 0;
+        for (j=0; j<childfac; j++) {
+            *(p+(fi++)) = ele;
+            for (k=0; k<nextc; k++) {
+                *(p+(fi++)) = *(childres+j*nextc+k);
+            }
+        }
+
+        /*
+        for (j=0; j<childfac*c; j++) {
+            printf("%d", *(p+j));
+        }
+        printf("\n");
+        */
+
+        /*
+        printf("%d\n", len);
+        */
+
+        arrptr[pi] = p;
+        arrlen[pi] = childfac;
+
+        nexts = nexts + 1;
+        nextn = nextn - 1;
+        pi++;
+    }
+
+    /*
+    for (j=0; j<4; j++) 
+        printf("%d", *(arrptr[1]+j));
+    printf("\n");
+    */
+
+    fi = 0;
+    fac = compute_com_count(n, c);
+    res = (int *)malloc(sizeof(int)*c*fac);
+    for (pi=0; pi<len; pi++) {
+        int tempfac = arrlen[pi];
+        for (j=0; j<tempfac; j++)
+            for (k=0; k<c; k++) { 
+                *(res+(fi++)) = *(arrptr[pi]+j*c+k);
+            }
+
+    }
+    
+
+    /*
+    for (i=0; i<c*fac; i++) 
+        printf("%d\n", *(res+i));
+     */
+
+    return res;
+}
 
 int compute_com_count(int n, int k) {
     int i = 1;
@@ -86,8 +177,9 @@ int compute_com_count(int n, int k) {
 }
 
 main() {
+        /*
     int maxlen = 16, i, rows=4, columns=4;
-    char source_data[maxlen]={0}, s[columns];	
+    char source_data[16]={0}, s[columns];	
 
     for (i=0; i<rows; i++) {  
         scanf("%s", s);
@@ -100,7 +192,9 @@ main() {
 	for (i=0; i<maxlen; i++) {
 		index[i] = i;
 	}
+    */
 
+    /*
 	int k,h;
     for (i=1; i<=maxlen; i++) {
         int fac_count = compute_com_count(maxlen, i);
@@ -121,6 +215,15 @@ main() {
 			}
 		}
     }
+    */
+
+    int *p = get_all_combine(4, 4, 4, 0);
+    int fac = compute_com_count(4, 4);
+    int i;
+    for (i=0; i<fac*4; i++)
+        printf("%d", *(p+i));
+
+    printf("\n");
     
-    printf("%s\n", source_data); 
+    //printf("%s\n", source_data); 
 }
